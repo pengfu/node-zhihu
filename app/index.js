19460529f03b051020ea5cb00201ee46dcd7840c
chang_su <chang_su@hunliji.com>
2020-12-06 14:40:34 +0800

    使用koa-json-error,并且在production环境不暴露错误堆栈信息

--- app/index.js
+++ app/index.js
@@ -1,9 +1,16 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const app = new Koa()
 const routing = require('./routes')
 const port = 3000
 
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === 'production' ? rest : { stack, ...rest },
+  })
+)
 app.use(bodyparser())
 routing(app)
 
