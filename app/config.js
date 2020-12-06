aa942bf19425d800c97d30d6f5244a902db507a8
chang_su <chang_su@hunliji.com>
2020-12-06 16:30:00 +0800

    连接mongodb云数据库

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    connectionStr: 'mongodb+srv://pengfu:pengfu123@node-zhihu.83pek.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- app/index.js
+++ app/index.js
@@ -2,9 +2,14 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
+const mongoose = require('mongoose')
 const app = new Koa()
 const routing = require('./routes')
 const port = 3000
+const { connectionStr } = require('./config')
+
+mongoose.connect(connectionStr, { useUnifiedTopology: true } ,() => console.log('mongodb 连接成功了'))
+mongoose.connection.on('error', console.error)
 
 app.use(
   error({
